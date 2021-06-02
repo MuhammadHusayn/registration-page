9df3c1a97f9f17f9d3a197d77d2fa3cba9c32ba4
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2021-06-02 10:21:05 +0500

    uploaded files

---
+++ reja.js
@@ -0,0 +1,21 @@
+/*
+	position 
+		absolute
+		relative
+		fixed
+		sticky
+
+	user chat mashqi
+	drop down menu
+
+	pseudo elements
+		before
+		after
+
+	user online exercise
+
+	vazifa
+		loop 2 step 2
+		loop 2 step 1
+
+*/
